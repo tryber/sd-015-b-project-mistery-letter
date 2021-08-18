ea9a51faa348ac5e04d332499c1c71d4620d5aa0
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-18 19:50:46 -0300

    Finalizado Requisito 5

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 //utils
 
-function generateLetter() {
-  const letterContainer = document.getElementById("carta-gerada");
+function checkIfHasLetter(letterContainer) {
   const createdLetter = document.getElementsByClassName = "letter-word";
   if (createdLetter) {
     letterContainer.innerHTML = "";
   }
-  const inputElement = document.getElementById("carta-texto");
-  const letterText = inputElement.value.split(" ");
+}
+
+function generateLetterElements(letterText, letterContainer) {
   for (let word of letterText) {
     let letterElement = document.createElement("span");
-    letterElement.className = "letter-word"
+    letterElement.className = "letter-word";
     letterElement.innerText = word;
     letterContainer.appendChild(letterElement);
+  }
+}
+
+function checkInputEmpty(inputElement, letterContainer) {
+  const inputText = inputElement.value.split(' ');
+  if (inputText.join('')) {
+    letterContainer.innerHTML = "";
+    return inputText;
+  } else {
+    letterContainer.innerText = "Por favor, digite o conteúdo da carta.";
+  }
+}
+
+function generateLetter() {
+  const letterContainer = document.getElementById("carta-gerada");
+  const inputElement = document.getElementById("carta-texto");
+  const letterText = checkInputEmpty(inputElement, letterContainer);
+  if (letterText) {
+    checkIfHasLetter(letterContainer);
+    generateLetterElements(letterText, letterContainer);
   }
 }
 
