ed5a4a9fb1479fc7575941565004cb58153be826
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-18 12:05:41 -0300

    Requisito 03

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+function createButton(buttonId, buttonText, insBeforeId) {
+  const button = document.createElement('button');
+  button.id = buttonId;
+  button.innerText = buttonText;
+  document.body.insertBefore(button, document.getElementById(insBeforeId));
+}
 
-
+function createLetterButton () {
+  createButton('criar-carta', 'Criar carta!', 'carta-gerada');
+}
+createLetterButton();
