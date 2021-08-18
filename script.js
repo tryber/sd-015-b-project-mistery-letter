8b883c6b6de5b3410f73bf2511ce702df959c10b
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-18 12:26:40 -0300

    Requisito 04

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const input = document.getElementById('carta-texto');
+const cartaGerada = document.getElementById('carta-gerada');
+
 function createButton(buttonId, buttonText, insBeforeId) {
   const button = document.createElement('button');
   button.id = buttonId;
@@ -5,7 +8,19 @@ function createButton(buttonId, buttonText, insBeforeId) {
   document.body.insertBefore(button, document.getElementById(insBeforeId));
 }
 
+function createLetter(event) {
+  const createSpan = document.createElement('span');
+  if (input.value === '') {
+    return alert ('por favor, digite o conteúdo da carta.')
+  }
+  createSpan.innerText = input.value;
+  cartaGerada.appendChild(createSpan);
+}
+
 function createLetterButton () {
   createButton('criar-carta', 'Criar carta!', 'carta-gerada');
+  const criarCarta = document.getElementById('criar-carta');
+  input.value = '';
+  criarCarta.addEventListener('click', createLetter);
 }
 createLetterButton();
