0e5d94f385033f56d63147d78cab0c639e4fe277
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-19 18:42:13 -0300

    Finalizados requisitos 16 e 17

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //utils
+function randomNumberGenerator(limit) {
+  const number = (Math.random() * limit);
+  const roundNumber = Math.round(number);
+  return roundNumber;
+}
+
+function addEventListenerToArray(array, event, func) {
+  for (let element of array) {
+    element.addEventListener(event, func);
+  }
+}
 
 function checkIfHasLetter(letterContainer) {
-  const createdLetter = document.getElementsByClassName = "letter-word";
+  const createdLetter = document.querySelector("#carta-gerada span");
   if (createdLetter) {
     letterContainer.innerHTML = "";
   }
@@ -10,7 +21,6 @@ function checkIfHasLetter(letterContainer) {
 function generateLetterElements(letterText, letterContainer) {
   for (let word of letterText) {
     let letterElement = document.createElement("span");
-    letterElement.className = "letter-word";
     letterElement.innerText = word;
     letterContainer.appendChild(letterElement);
   }
@@ -26,6 +36,41 @@ function checkInputEmpty(inputElement, letterContainer) {
   }
 }
 
+function generateStyleClassList() {
+  let styleGroup = ["newspaper", "magazine1", "magazine2"];
+  let sizeGroup = ["medium", "big", "reallybig"];
+  let rotationGroup = ["rotateleft", "rotateright"];
+  let skewGroup = ["skewleft", "skewright"];
+  let groupList = [styleGroup, sizeGroup, rotationGroup, skewGroup];
+  let styleClassList = [];
+  for (let group of groupList) {
+    randomClassNumber = randomNumberGenerator(group.length)
+    if (randomClassNumber) {
+      styleClassList.push(group[randomClassNumber - 1])
+      group = group.slice(randomClassNumber - 1)
+    }
+  }
+  return styleClassList
+}
+
+function addClassToElements() {
+  let wordsList = document.querySelectorAll("#carta-gerada span");
+  for (let word of wordsList) {
+    let styleClassList = generateStyleClassList();
+    for (let styleClass of styleClassList) {
+      word.classList.add(styleClass);
+    }
+  }
+}
+
+function resetLetterClasses() {
+  let wordsList = document.querySelectorAll("#carta-gerada span");
+  for (word of wordsList) {
+    word.className = ""
+  }
+  addClassToElements()
+}
+
 function generateLetter() {
   const letterContainer = document.getElementById("carta-gerada");
   const inputElement = document.getElementById("carta-texto");
@@ -33,14 +78,18 @@ function generateLetter() {
   if (letterText) {
     checkIfHasLetter(letterContainer);
     generateLetterElements(letterText, letterContainer);
+    addClassToElements();
   }
 }
 
 function addEventListeners() {
   const generateLetterButton = document.getElementById("criar-carta");
   generateLetterButton.addEventListener("click", generateLetter);
+  const letter = document.getElementById("carta-gerada");
+  letter.addEventListener("click", resetLetterClasses);
+
 }
 
-window.onload = function () {
+window.onload = function() {
   addEventListeners();
 }
