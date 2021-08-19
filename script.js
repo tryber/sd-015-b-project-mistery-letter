4866786734007af61acaf0157e9a2dc473d905bb
aaguero96 <andre.luiz_1996@hotmail.com>
2021-08-18 21:59:42 -0300

    Finish part 5

--- script.js
+++ script.js
@@ -6,11 +6,15 @@ const buttonToGenerateLetter = document.getElementById('criar-carta');
 // function to create span for any word in input id carta-texto
 function createSpanForInputWords() {
   const phrase = inputText.value;
-  const phraseArray = phrase.split(' ');
-  for (let i = 0; i < phraseArray.length; i += 1) {
-    const wordToSpan = document.createElement('span');
-    wordToSpan.innerText = phraseArray[i];
-    letterPhrase.appendChild(wordToSpan);
+  if (phrase.trim() !== '') {
+    const phraseArray = phrase.trim().split(' ');
+    for (let i = 0; i < phraseArray.length; i += 1) {
+      const wordToSpan = document.createElement('span');
+      wordToSpan.innerText = phraseArray[i];
+      letterPhrase.appendChild(wordToSpan);
+    }
+  } else {
+    letterPhrase.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
