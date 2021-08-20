723d64c095356464c9e0ab1bfe2d579894cde483
David Souza <dabcs@poli.ufrj.br>
2021-08-20 02:08:11 -0300

    adicao do contador

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const textLetter = document.getElementById('carta-gerada');
 const buttonLetter = document.getElementById('criar-carta');
 const input = document.getElementById('carta-texto');
 const selectSpan = document.querySelectorAll('span');
+const classStyle = ['newspaper', 'magazine1', 'magazine2'];
+const classSize = ['medium', 'big', 'reallybig'];
+const classRot = ['rotateleft', 'rotateright'];
+const classSkew = ['skewleft', 'kewright'];
+const count = document.getElementById('carta-contador');
 
 function correctSpan() {
   if (selectSpan) {
@@ -9,6 +14,24 @@ function correctSpan() {
       selectSpan[i].remove();
     }
   }
+}
+
+function generateClassString() {
+  const vector = [];
+  let classList = '';
+  for (let i = 0; i < 4; i += 1) {
+    if (vector.length < 2) {
+      vector.push(Math.floor(Math.random() * 3));
+    } else {
+      vector.push(Math.floor(Math.random() * 2));
+    }
+  }
+
+  classList = `${classStyle[vector[0]]} `;
+  classList = `${classList + classSize[vector[1]]} `;
+  classList = `${classList + classRot[vector[2]]} `;
+  classList += classSkew[vector[3]];
+  return classList;
 }
 
 function createLetter() {
@@ -20,10 +43,14 @@ function createLetter() {
     const stringLetter = input.value.split(' ');
     for (let i = 0; i < stringLetter.length; i += 1) {
       const span = document.createElement('span');
+      const classAdd = generateClassString();
+      span.className = classAdd;
       span.innerText = stringLetter[i];
       textLetter.appendChild(span);
     }
   }
+  count.innerText = document.querySelectorAll('span').length;
 }
 
 buttonLetter.addEventListener('click', createLetter);
+count.innerText = document.querySelectorAll('span').length;
