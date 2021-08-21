e1d8622635d99d87a542115a8735fb21cfe07bc2
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-21 17:48:44 -0300

    Requisitos 16 e 17 concluídos

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 const input = document.getElementById('carta-texto');
 const btnCriarCarta = document.getElementById('criar-carta');
 const paragrafoResultado = document.getElementById('carta-gerada');
+const estilo = ['newspaper', 'magazine1', 'magazine2'];
+const tamanho = ['medium', 'big', 'reallybig'];
+const rotacao = ['rotateleft', 'rotateright'];
+const inclinacao = ['skewleft', 'skewright'];
+
+function randomNumber(value) {
+  const result = Math.floor(Math.random() * (value + 1));
+  return result;
+}
 
 function gerarCarta() {
   paragrafoResultado.innerHTML = '';
   const textoInserido = input.value.split(' ');
 
   textoInserido.forEach((palavra) => {
+    const estiloAleatorio = estilo[randomNumber(2)];
+    const tamanhoAleatorio = tamanho[randomNumber(2)];
+    const rotacaoAleatoria = rotacao[randomNumber(1)];
+    const inclinacaoAleatoria = inclinacao[randomNumber(1)];
     const span = document.createElement('span');
     span.innerText = palavra;
+    span.classList.add(estiloAleatorio);
+    span.classList.add(tamanhoAleatorio);
+    span.classList.add(rotacaoAleatoria);
+    span.classList.add(inclinacaoAleatoria);
     paragrafoResultado.appendChild(span);
   });
 }
