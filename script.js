26b2b4c51137159225c2474eb2b852519b81be14
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-22 01:39:48 -0300

    Add exercise 16

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const input = document.getElementById('carta-texto');
+const body = document.querySelector('body');
+const input = document.querySelector('input');
 const cartaGerada = document.getElementById('carta-gerada');
 
 function createButton(buttonId, buttonText, insBeforeId) {
@@ -8,19 +9,51 @@ function createButton(buttonId, buttonText, insBeforeId) {
   document.body.insertBefore(button, document.getElementById(insBeforeId));
 }
 
-function createLetter(event) {
-  const createSpan = document.createElement('span');
-  if (input.value === '') {
-    return alert ('por favor, digite o conteúdo da carta.')
+function getRandomClasses() {
+  const styleClasses = ['newspaper', 'magazine1', 'magazine2'];
+  const sizeClasses = ['medium', 'big', 'reallybig'];
+  const rotationClasses = ['rotateleft', 'rotateright'];
+  const skewClasses = ['skewleft', 'skewright'];
+  let classList = [];
+  for (let count = 0; count < 1; count += 1) {
+    classList += styleClasses[Math.floor(Math.random() * styleClasses.length)];
+    classList += ' ' + sizeClasses[Math.floor(Math.random() * sizeClasses.length)];
   }
-  createSpan.innerText = input.value;
-  cartaGerada.appendChild(createSpan);
+  for (let count = 0; count < 1; count += 1) {
+    classList += ' ' + rotationClasses[Math.floor(Math.random() * rotationClasses.length)];
+    classList += ' ' + skewClasses[Math.floor(Math.random() * skewClasses.length)];
+  }
+  return classList;
 }
 
-function createLetterButton () {
+const cartaContador = document.querySelector('#carta-contador');
+let wordCounter = 0;
+
+function createLetter() {
+  if (input.value === '' || input.value < ' ') {
+    return alert('por favor, digite o conteúdo da carta.');
+  }
+  const space = ' ';
+  const words = input.value.split(space);
+  for (let word = 0; word < words.length; word += 1) {
+    const createSpan = document.createElement('span');
+    createSpan.innerText = words[word] + ' ';
+    createSpan.className = getRandomClasses();
+    cartaGerada.appendChild(createSpan);
+  }
+}
+
+function createLetterButton() {
   createButton('criar-carta', 'Criar carta!', 'carta-gerada');
   const criarCarta = document.getElementById('criar-carta');
   input.value = '';
   criarCarta.addEventListener('click', createLetter);
+  input.addEventListener('keypress', checkEnterPressed);
 }
 createLetterButton();
+
+function checkEnterPressed(event) {
+  if (event.key === 'Enter') {
+    createLetter();
+  }
+}
