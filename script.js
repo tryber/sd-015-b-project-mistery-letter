0a23f4371e0b3980cf7f4ab5020005ff5bd263dc
Paulo Flóra <tflora.2004@gmail.com>
2021-08-23 02:54:57 -0300

    Feito o requisito 16.

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const letterContainer = document.getElementById('carta-gerada');
 const letterText = document.getElementById('carta-texto');
 const letterBtn = document.getElementById('criar-carta');
 
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotation = ['rotateleft', 'rotateright'];
+const skew = ['skewleft', 'skewright'];
+
+function oneOfThreeClasses() {
+  const rdm = Math.floor(Math.random() * 3);
+  return rdm;
+}
+
+function oneOfTwoClasses() {
+  const rdm = Math.floor(Math.random() * 2);
+  return rdm;
+}
+
+function addClasses() {
+  const wordList = document.getElementsByTagName('span');
+  for (let i = 0; i < wordList.length; i += 1) {
+    const newClass1 = style[oneOfThreeClasses()];
+    const newClass2 = size[oneOfThreeClasses()];
+    const newClass3 = rotation[oneOfTwoClasses()];
+    const newClass4 = skew[oneOfTwoClasses()];
+    wordList[i].classList.add(newClass1);
+    wordList[i].classList.add(newClass2);
+    wordList[i].classList.add(newClass3);
+    wordList[i].classList.add(newClass4);
+  }
+}
+
 function generateLetter() {
   if (letterText.value.trim() === '') {
     letterContainer.innerText = 'Por favor, digite o conteúdo da carta.';
@@ -14,6 +43,7 @@ function generateLetter() {
       letterContainer.appendChild(newSpan);
     }
   }
+  addClasses();
 }
 
 letterBtn.addEventListener('click', generateLetter);
