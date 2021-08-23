8d04bd308555c9bdd3df24e5f21ec8ea3e3cdd70
Paulo Flóra <tflora.2004@gmail.com>
2021-08-23 02:13:01 -0300

    Feito o requisito 5.

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const letterText = document.getElementById('carta-texto');
 const letterBtn = document.getElementById('criar-carta');
 
 function generateLetter() {
-  letterContainer.innerHTML = '';
-  const letterWords = letterText.value.split(' ');
-  for (let i = 0; i < letterWords.length; i += 1) {
-    const newSpan = document.createElement('span');
-    newSpan.innerText = `${letterWords[i]}`;
-    letterContainer.appendChild(newSpan);
+  if (letterText.value.trim() === '') {
+    letterContainer.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    letterContainer.innerHTML = '';
+    const letterWords = letterText.value.split(' ');
+    for (let i = 0; i < letterWords.length; i += 1) {
+      const newSpan = document.createElement('span');
+      newSpan.innerText = `${letterWords[i]}`;
+      letterContainer.appendChild(newSpan);
+    }
   }
 }
 
