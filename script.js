bd65165ded8201359cd52b676a4c309ce525c8ff
Flávio Bianchetti <fjb.developer@gmail.com>
2021-08-24 14:16:03 -0300

    Atendidos requisitos 16 e 17 - validado via npm

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const cartaTexto = document.getElementById('carta-texto');
 const cartaGerada = document.getElementById('carta-gerada');
 const criarCarta = document.getElementById('criar-carta');
+const groupStyle = ['newspaper', 'magazine1', 'magazine2'];
+const groupHeight = ['medium', 'big', 'reallybig'];
+const groupRotation = ['rotateleft', 'rotateright'];
+const groupInclination = ['skewleft', 'skewright'];
 
 function splitLetter(myLetter) {
   const arrayCartaTexto = myLetter.split(' ');
@@ -13,6 +17,25 @@ function splitLetter(myLetter) {
   return arrayValid;
 }
 
+function randomNumberStyle(num) {
+  let numberStyle = Math.floor((Math.random()) * num);
+  if (numberStyle === num) {
+    numberStyle = num - 1;
+  }
+  return numberStyle;
+}
+
+function randomClassStyle() {
+  let newSpanClass = groupStyle[randomNumberStyle(3)];
+  newSpanClass += ' ';
+  newSpanClass += groupHeight[randomNumberStyle(3)];
+  newSpanClass += ' ';
+  newSpanClass += groupRotation[randomNumberStyle(2)];
+  newSpanClass += ' ';
+  newSpanClass += groupInclination[randomNumberStyle(2)];
+  return newSpanClass;
+}
+
 function newLetters() {
   const arrayCartaTexto = splitLetter(cartaTexto.value);
   console.log(arrayCartaTexto.length);
@@ -22,6 +45,7 @@ function newLetters() {
     for (let index = 0; index < arrayCartaTexto.length; index += 1) {
       const newSpan = document.createElement('span');
       newSpan.innerText = arrayCartaTexto[index];
+      newSpan.className = randomClassStyle();
       cartaGerada.appendChild(newSpan);
     }
   }
