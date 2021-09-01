065eb5cf629844c30882ba4b7596aac760a50d01
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-01 01:10:37 -0300

    Finaliza requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
+const tagSpanPai = document.getElementById('carta-gerada');
+
+function geraCartaNucleo(textoSeparado) {
+  for (let i = 0; i < textoSeparado.length; i += 1) {
+    const tagSpan = document.createElement('span');
+    tagSpan.innerText = textoSeparado[i];
+    tagSpanPai.appendChild(tagSpan);
+  }
+}
+function geraCarta(textoSeparado) {
+  const tagSpanExistente = document.getElementsByTagName('span');
+
+  if (tagSpanExistente.length === 0) {
+    geraCartaNucleo(textoSeparado);
+  }
+
+  for (let i = 0; i < textoSeparado.length; i += 1) {
+    if (textoSeparado[i] !== tagSpanExistente[i].innerText) {
+      geraCartaNucleo(textoSeparado);
+    }
+  }
+}
+
 function criaCartaMisteriosa() {
   const texto = document.getElementById('carta-texto');
   const textoSeparado = texto.value.split(' ');
-  for (let i = 0; i < textoSeparado.length; i += 1) {
-    const tagSpan = document.createElement('span');
-    const tagSpanPai = document.getElementById('carta-gerada');
-    tagSpan.innerText = textoSeparado[i];
-    tagSpanPai.appendChild(tagSpan);
+  const elementoP = document.getElementById('carta-gerada');
+  if (texto.value === '') {
+    elementoP.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    elementoP.innerText = '';
+    geraCarta(textoSeparado);
   }
 }
 
